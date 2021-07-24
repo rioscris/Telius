977cb64e985c97d1a1dfeae47e97291b36ccba31
rios cristian <cristian.rios@moxit.net>
2021-07-24 18:41:10 -0300

    Better formatting

--- telius-app/src/features/main/menu.jsx
+++ telius-app/src/features/main/menu.jsx
@@ -7,42 +7,50 @@ import Searcher from "./Searcher";
 import "./main.css";
 
 export const Menu = () => {
-    return (
-        <Box className='main'>
-            <Grid h="100px" templateColumns="repeat(7, 1fr)" gap={4}>
-                <GridItem colSpan={1} >
-                    <Center>
-                        <Image marginLeft={'1em'} marginRight={'1em'} src={TeliusLogo} alt="telius" boxSize='4em' />
-                        <Heading
-                            style={{
-                                color: "white",
-                                fontFamily: "Varela Round",
-                            }}
-                        >Telius</Heading>
-                    </Center>
-                </GridItem>
-                <GridItem colSpan={7} rowSpan={5}>
-                    <Center height={"100%"} w="100%">
-                        <Heading
-                            size="2xl"
-                            style={{
-                                color: "white",
-                                fontFamily: "Varela Round",
-                            }}
-                            textAlign="center"
-                            lineHeight="1.4"
-                        >
-                            Encontrá el profesor más cercano a tu casa
-                            <br />y comenzá tu aprendizaje
-                        </Heading>
-                    </Center>
-                </GridItem>
-                <GridItem colSpan={7} rowSpan={6}>
-                    <Searcher />
-                </GridItem>
-            </Grid>
-        </Box>
-    );
+  return (
+    <Box className="main">
+      <Grid h="100px" templateColumns="repeat(7, 1fr)" gap={4}>
+        <GridItem colSpan={1}>
+          <Center>
+            <Image
+              marginLeft={"1em"}
+              marginRight={"1em"}
+              src={TeliusLogo}
+              alt="telius"
+              boxSize="4em"
+            />
+            <Heading
+              style={{
+                color: "white",
+                fontFamily: "Varela Round",
+              }}
+            >
+              Telius
+            </Heading>
+          </Center>
+        </GridItem>
+        <GridItem colSpan={7} rowSpan={5}>
+          <Center height={"100%"} w="100%">
+            <Heading
+              size="2xl"
+              style={{
+                color: "white",
+                fontFamily: "Varela Round",
+              }}
+              textAlign="center"
+              lineHeight="1.4"
+            >
+              Encontrá el profesor más cercano a tu casa
+              <br />y comenzá tu aprendizaje
+            </Heading>
+          </Center>
+        </GridItem>
+        <GridItem colSpan={7} rowSpan={6}>
+          <Searcher />
+        </GridItem>
+      </Grid>
+    </Box>
+  );
 };
 
 export default Menu;
